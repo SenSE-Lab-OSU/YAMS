5de4b69d74ad7b45ecf202133484467a029748ee
yuyichang <22040659+YuyiChang@users.noreply.github.com>
2025-02-28 11:33:04 -0500

    separate install and run bat scripts

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,5 +1,7 @@
 python -m venv .venv
 call .venv\Scripts\activate
-python -m pip install -U yams-util 
 python -m yams
 deactivate
+
+echo Program exited. Press any key to close this window...
+pause >nul
