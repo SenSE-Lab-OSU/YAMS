6b226e7d4dc48deb695f519869ad68d54d19f848
yuyichang <22040659+YuyiChang@users.noreply.github.com>
2025-04-10 18:09:32 -0400

    prep for v2.0.1

--- scripts/windows/install.bat
+++ scripts/windows/install.bat
@@ -1,7 +1,6 @@
 python -m venv .venv
 call .venv\Scripts\activate
 python -m pip install -U yams-util 
-python -m pip install matplotlib
 
 echo Install/update complete. Press any key to exit...
 pause
