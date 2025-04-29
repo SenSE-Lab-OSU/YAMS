8dc5dfb96b0c0f08b07eedeb20c2fc9ef7d52e08
yuyichang <22040659+YuyiChang@users.noreply.github.com>
2025-04-28 22:46:16 -0400

    add enmo experimental

--- yams/bluetooth_device.py
+++ yams/bluetooth_device.py
@@ -28,7 +28,22 @@ async def get_device_status(addr):
         status = await client.read_gatt_char(characteristic_collection)
         info['status'] = str(status[0])
 
+        # enmo = await client.read_gatt_char("da39c951-1d81-48e2-9c68-d0ae4bbd351f")
+        # info['enmo'] = enmo.decode('utf-8')
+        await client.start_notify("da39c951-1d81-48e2-9c68-d0ae4bbd351f", enmo_handler)
+
+        await asyncio.sleep(10)  # Listen for 10 seconds
+        await client.stop_notify("da39c951-1d81-48e2-9c68-d0ae4bbd351f")
+
     return info
+
+def enmo_handler(sender, data):
+    packet_counter = data[4:6]
+    ENMO = struct.unpack("<f", data[0:4])
+    
+    packet_counter = struct.unpack("<H", packet_counter)
+    horizontal_array = [ENMO[0], packet_counter[0]]
+    print("package counter", str(horizontal_array))
 
 def status_updater():
     import random
