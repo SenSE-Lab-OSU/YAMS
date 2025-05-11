b4f5ea8c10a076bdec801cd9a6f07d371e7f11b8
yuyichang <22040659+YuyiChang@users.noreply.github.com>
2025-05-11 12:52:53 -0400

    add lsl support, update UX and flow in msense ctl

--- yams/msense_collector.py
+++ yams/msense_collector.py
@@ -8,7 +8,32 @@ import struct
 from functools import partial
 from apscheduler.schedulers.background import BackgroundScheduler
 import hashlib
-# from yams.bt_scanner import get
+from pylsl import StreamInfo, StreamOutlet
+import os
+import numpy as np
+
+class MsenseOutlet(StreamOutlet):
+    def __init__(self, name, peripheral, chunk_size=32, max_buffered=360):
+        self.name = name
+        info = StreamInfo(name, "MotionSenSE", 2, 25, "float32", peripheral.address())
+        super().__init__(info, chunk_size, max_buffered)
+
+        self.log_dir = os.path.join("log", "default")
+
+    def save_data(self, data):
+        self.log_path = os.path.join(self.log_dir, f"{self.name}.txt")
+        # Ensure the file exists
+        if not os.path.exists(self.log_path):
+            with open(self.log_path, 'w') as f: pass
+
+        # Append NumPy array as a line
+        with open(self.log_path, 'a') as f:
+            np.savetxt(f, [data], fmt='%s')
+
+    def push_sample(self, x):
+        self.save_data(x)
+        return super().push_sample(x)
+
 
 class MsenseController():
     def __init__(self):
@@ -17,6 +42,7 @@ class MsenseController():
         self.device_name = self.get_dev_dict()
         self.init_adapter()
         self.active_devices = {}
+        self.active_outlets = {}
 
         self.scheduler = BackgroundScheduler()
 
@@ -65,9 +91,12 @@ class MsenseController():
     def connect_devices(self, names):
         del(self.active_devices)
         self.active_devices = {}
+        del(self.active_outlets)
+        self.active_outlets = {}
 
         self.log(f"Start connecting to devices: {names}")
         for n in names:
+            gr.Info(f"Connecting to devices: {n}")
             print(f'==== {n}')
             p = self.devices[n]
             print(f"=== {p.identifier()} at {p.address()}")
@@ -75,6 +104,7 @@ class MsenseController():
             p.set_callback_on_disconnected(lambda: print(f"{self.tic()}: [INFO] {p.identifier()} is disconnected"))
             p.connect()
             self.active_devices[n] = p
+            self.active_outlets[n] = MsenseOutlet(n, p)
 
     def disconnect_all(self):
         for dev in self.active_devices.values():
@@ -82,6 +112,7 @@ class MsenseController():
                 dev.disconnect()
             except Exception as e:
                 print(str(e))
+        gr.Warning(f"All devices disconnected")
 
     def tic(self):
         return datetime.datetime.now()
@@ -90,18 +121,14 @@ class MsenseController():
         print(f"{self.tic()}: {msg}")
 
     def interface(self):
-        with gr.Row():
-            text = gr.Text("MSense", label="Device filter", scale=2)
+        with gr.Accordion("Initialization"):
             bt_search = gr.Button("Search Bluetooth devices 📱")
+            available_devices = gr.CheckboxGroup(label="Available devices", scale=6)
+            with gr.Row():
+                bt_connect = gr.Button("✅ Connect selected")
+                btn_disconnect = gr.Button("❌ Disconnect")
 
-        with gr.Row():
-            with gr.Column(scale=4):
-                available_devices = gr.CheckboxGroup(label="Available devices")
-            with gr.Column():
-                bt_connect = gr.Button("Connect selected")
-                btn_disconnect = gr.Button("Disconnect")
-
-        bt_search.click(self.get_available_devices_checkbox, inputs=[text], outputs=available_devices)    
+        
         bt_connect.click(self.connect_devices, inputs=available_devices)            
         btn_disconnect.click(self.disconnect_all)
 
@@ -122,7 +149,12 @@ class MsenseController():
                 btn_start.click(self.start_collection)
                 btn_stop.click(self.end_collection)
 
-        with gr.Accordion(label="Advanced options", open=True):
+        with gr.Accordion(label="Advanced options", open=False):
+            text = gr.Text("MSense", label="Device filter", scale=2)
+
+            auto_reconnect = gr.Checkbox(True, label="Auto reconnect")
+            auto_reconnect.change(self.set_auto_reconnect, inputs=auto_reconnect)
+
             btn_reconnect = gr.Button("Reconnect")
             btn_reconnect.click(self.reconnect)
 
@@ -135,21 +167,37 @@ class MsenseController():
                 btn_monitor_start.click(self.start_device_monitor)
                 btn_monitor_stop.click(self.stop_device_monitor)
 
+        bt_search.click(self.get_available_devices_checkbox, inputs=text, outputs=available_devices)    
+
+    def set_auto_reconnect(self, status):
+        self.auto_reconnect = status
+
     def start_collection(self):
+        # create log dir
+        self.log_dir = os.path.join('log', self.session_info['sub_id'], self.session_info['ses_id'], time.strftime("%y%m%d_%H%M"))
+        print(f"create log dir {self.log_dir}")
+        os.makedirs(self.log_dir, exist_ok=True)
+
+        gr.Info("▶️ Start data collection...")
+
         for name, p in self.active_devices.items():
             print(name, p.is_connected(), p.is_connectable())
-            self.collection_ctl(p, True)
+            self.collection_ctl(name, True)
+
+            self.active_outlets[name].log_dir = self.log_dir
 
     def end_collection(self):
+        gr.Info("🛑 Stop data collection...")
         for name, p in self.active_devices.items():
             print(name, p.is_connected(), p.is_connectable())
-            self.collection_ctl(p, False)
+            self.collection_ctl(name, False)
     
-    def collection_ctl(self, peripheral, start=True):
+    def collection_ctl(self, name, start=True):
+        peripheral = self.active_devices[name]
+
         # if starting, do the initialization
         if start:
             # write unix time
-
             print("==== writing", int(time.time()), time.time())
             peripheral.write_request("da39c930-1d81-48e2-9c68-d0ae4bbd351f", 
                                      "da39c932-1d81-48e2-9c68-d0ae4bbd351f", 
@@ -163,19 +211,32 @@ class MsenseController():
         characteristic_uuid = "da39c931-1d81-48e2-9c68-d0ae4bbd351f"
         peripheral.write_request(service_uuid, characteristic_uuid, struct.pack("<I", int(start)))
 
+        self.register_enmo(peripheral, name)
+
+        # 
+        if start and self.auto_reconnect:
+            self.start_device_monitor()
+        elif not start:
+            self.stop_device_monitor()
+            
+
+    def register_enmo(self, peripheral, name):
         # ENMO 
         service_uuid = "da39c950-1d81-48e2-9c68-d0ae4bbd351f"
         characteristic_uuid = "da39c951-1d81-48e2-9c68-d0ae4bbd351f"
-        contents = peripheral.notify(service_uuid, characteristic_uuid, lambda data: self.enmo_handler(data, peripheral))
+        contents = peripheral.notify(service_uuid, characteristic_uuid, lambda data: self.enmo_handler(data, peripheral, name))
 
-    def enmo_handler(self, data, peripheral):
-        print(peripheral.identifier(), data)
+    def enmo_handler(self, data, peripheral, name):
+        # print(peripheral.identifier(), data)
         packet_counter = data[4:6]
         ENMO = struct.unpack("<f", data[0:4])
         
         packet_counter = struct.unpack("<H", packet_counter)
         horizontal_array = [ENMO[0], packet_counter[0]]
-        print("package counter", str(horizontal_array))
+        print(f"{name}: package counter", horizontal_array)
+
+        self.active_outlets[name].push_sample([ENMO[0], packet_counter[0]])
+        
 
     def get_selected_device_services(self, names):
         for n in names:
@@ -199,18 +260,20 @@ class MsenseController():
             if not p.is_connected():
                 try:
                     p.connect()
+                    self.register_enmo(p, name)
                 except Exception as e:
                     print(str(e))
 
     ######
     def check_and_reconnect_devices(self):
-        for device in self.active_devices.values():
+        for name, device in self.active_devices.items():
             try:
                 if not device.is_connected():
                     print(f"[INFO] {device.identifier()} disconnected. Attempting to reconnect...")
                     device.connect()
                     if device.is_connected():
                         print(f"[INFO] Reconnected to {device.identifier()}")
+                        self.register_enmo(device, name)
                     else:
                         print(f"[WARN] Failed to reconnect to {device.identifier()}")
                 else:
@@ -238,52 +301,11 @@ class MsenseController():
         integer_representation = int(hex_digest, 16) % 32000
         print(name, integer_representation)
         self.participant_byte = struct.pack("<I", integer_representation)
+
+        self.session_info = {
+            'sub_id': sub,
+            'ses_id': ses,
+            'participant_enc': integer_representation
+        }
         return integer_representation
 
-
-if __name__ == "__main__":
-    adapters = simplepyble.Adapter.get_adapters()
-
-    if len(adapters) == 0:
-        print("No adapters found")
-
-    # Query the user to pick an adapter
-    print("Please select an adapter:")
-    for i, adapter in enumerate(adapters):
-        print(f"{i}: {adapter.identifier()} [{adapter.address()}]")
-
-    choice = int(input("Enter choice: "))
-    adapter = adapters[choice]
-
-    print(f"Selected adapter: {adapter.identifier()} [{adapter.address()}]")
-
-    adapter.set_callback_on_scan_start(lambda: print("Scan started."))
-    adapter.set_callback_on_scan_stop(lambda: print("Scan complete."))
-    adapter.set_callback_on_scan_found(lambda peripheral: print(f"Found {peripheral.identifier()} [{peripheral.address()}]"))
-
-    # Scan for 5 seconds
-    adapter.scan_for(5000)
-    peripherals = adapter.scan_get_results()
-
-    # Query the user to pick a peripheral
-    print("Please select a peripheral:")
-    for i, peripheral in enumerate(peripherals):
-        print(f"{i}: {peripheral.identifier()} [{peripheral.address()}]")
-
-    choice = int(input("Enter choice: "))
-    peripheral = peripherals[choice]
-
-    print(f"Connecting to: {peripheral.identifier()} [{peripheral.address()}]")
-    peripheral.connect()
-
-    print("Successfully connected, listing services...")
-    services = peripheral.services()
-    for service in services:
-        print(f"Service: {service.uuid()}")
-        for characteristic in service.characteristics():
-            print(f"    Characteristic: {characteristic.uuid()}")
-
-            capabilities = " ".join(characteristic.capabilities())
-            print(f"    Capabilities: {capabilities}")
-
-    peripheral.disconnect()
