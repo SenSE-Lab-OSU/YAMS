da430c942e9a4a9e14113addd2643e12f22da985
yuyichang <22040659+YuyiChang@users.noreply.github.com>
2026-09-03 19:24:58 -0400

    v2 ui and documentation. rm v1 modules

--- yams/e_stop.py
+++
@@ -1,8 +0,0 @@
-from yams.bt_scanner import bleak_scan, collection_ctl, device_info
-import asyncio
-
-if __name__ == '__main__':
-    asyncio.run(bleak_scan("MSense"))
-    collection_ctl(device_info.keys(), False)
-
-    print("All devices stop")
